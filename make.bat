339b22e8b7b0fa24ad8a5e8b90006c87e3376f11
petercommand <petercommand@gmail.com>
2016-05-07 11:30:59 +0800

    fix typo

--- make.bat
+++ make.bat
@@ -4,7 +4,7 @@ SET INCLUDELIBS="Kernel32.lib user32.lib"
 SET ASEMBLER=ML.exe
 SET ASEMBLERPARAM="/c /coff /Zi /Fl"
 SET LINKER=LINK.exe
-SET LINKERPARAM="/deubg /subsystem:console /entry:%ENTRY% /out:%ENTRY%.exe %INCLUDELIBS%"
+SET LINKERPARAM="/debug /subsystem:console /entry:%ENTRY% /out:%ENTRY%.exe %INCLUDELIBS%"
 
 REM submodule sentiment_analysis
 sentiment_analysis/make.bat
