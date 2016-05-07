ec4fb0bdab679b382f8e7066d0549676cff3b78c
petercommand <petercommand@gmail.com>
2016-05-07 11:29:55 +0800

    add initial build file

---
+++ make.bat
@@ -0,0 +1,10 @@
+@echo off
+SET ENTRY=main
+SET INCLUDELIBS="Kernel32.lib user32.lib"
+SET ASEMBLER=ML.exe
+SET ASEMBLERPARAM="/c /coff /Zi /Fl"
+SET LINKER=LINK.exe
+SET LINKERPARAM="/deubg /subsystem:console /entry:%ENTRY% /out:%ENTRY%.exe %INCLUDELIBS%"
+
+REM submodule sentiment_analysis
+sentiment_analysis/make.bat
